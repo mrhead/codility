1dfc9dcc0cc8bcda4d4e0459161bbc43ba4116fd
Patrik Bóna <patrik.bona@mrhead.sk>
2014-05-29 17:10:23 +0200

    Lesson 2 - FrogRiverOne (from scratch)

--- frog_river_one.rb
+++ frog_river_one.rb
@@ -1,9 +1,9 @@
 def frog_river_one(x, a)
-  seen = {}
+  fallen_leaves = {}
 
-  a.each_index do |i|
-    seen[a[i]] = true
-    return i if seen.size == x
+  a.each_with_index do |leaf, i|
+    fallen_leaves[leaf] = true
+    return i if fallen_leaves.size == x
   end
 
   -1
@@ -16,7 +16,11 @@ class Tests < MiniTest::Unit::TestCase
     assert_equal 6, frog_river_one(5, [1, 3, 1, 4, 2, 3, 5, 4])
   end
 
-  def test_never
-    assert_equal(-1, frog_river_one(2, [1,1,1]))
+  def test_not_possible
+    assert_equal -1, frog_river_one(5, [1, 2, 1, 2, 1, 4, 5])
+  end
+
+  def test_immediately
+    assert_equal 0, frog_river_one(1, [1, 1])
   end
 end
