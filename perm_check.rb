4b73b552b78d1c291a03883359b443cc1dd752ee
Patrik Bóna <patrik.bona@mrhead.sk>
2014-05-29 16:47:53 +0200

    Lesson 2 - PermCheck (from scratch)

--- perm_check.rb
+++ perm_check.rb
@@ -1,21 +1,39 @@
 def perm_check(a)
-  sorted_a = a.sort
+  seen = {}
 
-  sorted_a.each_index do |i|
-    return 0 if sorted_a[i] != i+1
+  max = a.max
+  a.each do |v|
+    seen[v] = true
   end
 
-  return 1
+  seen.size == max && max == a.size ? 1 : 0
+
+  # alternative solution
+  #
+  # seen = {}
+  #
+  # a.each do |v|
+  #   seen[v] = true
+  # end
+  #
+  # a.size.times do |n|
+  #   return 0 if seen[n+1].nil?
+  # end
+  # 1
 end
 
 require 'minitest/autorun'
 
 class Tests < MiniTest::Unit::TestCase
-  def test_example_one
-    assert_equal 1, perm_check([4,1,3,2])
+  def test_example_input
+    assert_equal 1, perm_check([4, 1, 3, 2])
   end
 
-  def test_example_two
-    assert_equal 0, perm_check([4,1,3])
+  def test_no_perm
+    assert_equal 0, perm_check([4, 1, 3])
+  end
+
+  def test_same_numbers
+    assert_equal 0, perm_check([1, 1, 2])
   end
 end
