b02f7fc75a9b2719fba3ed3b1b60f3bae73ef96c
Patrik Bóna <patrik.bona@mrhead.sk>
2014-05-30 15:25:33 +0200

    Lesson 4 - MaxProductOfThree (from scratch)

--- max_product_of_three.rb
+++ max_product_of_three.rb
@@ -1,11 +1,13 @@
 def max_product_of_three(a)
   a.sort!
 
-  two_negatives = a[0] * a[1] * a.last
-  biggest_numbers = a.last(3).inject(:*)
+  # three biggest numbers
+  tmp1 = a.last(3).inject(:*)
 
-  return two_negatives if two_negatives > biggest_numbers
-  biggest_numbers
+  # two smallest negative numbers and biggest positive number
+  tmp2 = a[0] * a[1] * a[-1]
+
+  [tmp1, tmp2].max
 end
 
 require 'minitest/autorun'
@@ -15,7 +17,15 @@ class Tests < MiniTest::Unit::TestCase
     assert_equal 60, max_product_of_three([-3, 1, 2, -2, 5, 6])
   end
 
-  def test_negative_values
-    assert_equal 1000, max_product_of_three([-10, -10, 1, 10])
+  def test_negative
+    assert_equal 30, max_product_of_three([-1, -10, 2, 3])
+  end
+
+  def test_all_negative
+    assert_equal -1, max_product_of_three([-1, -1, -1, -2])
+  end
+
+  def test_zero
+    assert_equal 0, max_product_of_three([-1, -1, -1, 0])
   end
 end
