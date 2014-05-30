3ccc6c484157e454279f311c5e8f3fc7b72ec95b
Patrik Bóna <patrik.bona@mrhead.sk>
2014-05-30 10:08:05 +0200

    Lesson 3 - PassingCars (from scratch)

--- passing_cars.rb
+++ passing_cars.rb
@@ -1,45 +1,39 @@
 def passing_cars(a)
-  passing_cars = 0
-  max_passing_cars = 1000000000
-  suffix_sums = []
   n = a.size
+  passing_cars = 0
 
-  a.each_index do |i|
-    index = n - i - 1
-    prev = suffix_sums[index+1].nil? ? 0 : suffix_sums[index+1]
-    suffix_sums[index] = a[index] + prev
+  suffix_sums = Array.new(n + 1, 0)
+
+  a.reverse.each_with_index do |v, i|
+    suffix_sums[i + 1] = suffix_sums[i] + v
+  end
+  suffix_sums.reverse!
+
+  a.each_with_index do |car, i|
+    if car == 0
+      passing_cars += suffix_sums[i]
+    end
   end
 
-  a.each_index do |i|
-    passing_cars += suffix_sums[i] if a[i] == 0
-    return -1 if passing_cars > max_passing_cars
-  end
-
-  passing_cars
+  passing_cars > 1_000_000_000 ? -1 : passing_cars
 end
 
 require 'minitest/autorun'
 
 class Tests < MiniTest::Unit::TestCase
-  def test_one_passing_car
-    assert_equal 1, passing_cars([0, 1])
-  end
-
-  def test_no_passing_car
-    assert_equal 0, passing_cars([1])
-  end
-
   def test_example_input
     assert_equal 5, passing_cars([0, 1, 0, 1, 1])
   end
 
-  def test_bilion_passing_cars
-    cars = Array.new(10000, 0) + Array.new(100000, 1)
-    assert_equal 1000000000, passing_cars(cars)
+  def test_no_passing_cars
+    assert_equal 0, passing_cars([1, 1, 1, 0, 0, 0])
   end
 
-  def test_more_than_bilion_passing_cars
-    cars = Array.new(10001, 0) + Array.new(100000, 1)
-    assert_equal -1, passing_cars(cars)
+  def test_all_passing
+    assert_equal 9, passing_cars([0, 0, 0, 1, 1, 1])
+  end
+
+  def test_a_lot_of_passing_cars
+    assert_equal -1, passing_cars(Array.new(100_000, 0) + Array.new(10_001, 1))
   end
 end
