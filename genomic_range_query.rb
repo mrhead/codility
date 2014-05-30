69a492da06c4e0b3442cd55f06459428df60ad18
Patrik Bóna <patrik.bona@mrhead.sk>
2014-05-30 13:25:23 +0200

    Lesson 3 - GenomicRangeQuery (from scratch)
    I fing previous solution a little bit more elegant. But this scores
    100% too.

--- genomic_range_query.rb
+++ genomic_range_query.rb
@@ -1,30 +1,33 @@
-# I was not able to do this in O(N+M) by myself ;(
-# But at least I learned something new when I "reimplemented":
-# https://github.com/jsuchal/codility/blob/master/genomic_range_query.rb
-
 def genomic_range_query(s, p, q)
-  letters = %w{A C G T}
+  letters = { 'A' => 1, 'C' => 2, 'G' => 3, 'T' => 4 }
   n = s.size
+
   prefix_sums = Hash.new { |h, k| h[k] = Array.new(n + 1, 0) }
 
-  s.chars.each_with_index do |char, i|
-    letters.each do |letter|
-      prefix_sums[letter][i+1] = prefix_sums[letter][i]
+  letters.keys.each do |letter|
+    for i in 1..n
+      prefix_sums[letter][i] = prefix_sums[letter][i - 1]
+      prefix_sums[letter][i] += 1 if s[i-1] == letter
     end
-    prefix_sums[char][i+1] = prefix_sums[char][i] + 1
   end
 
-  p.zip(q).map do |from, to|
-    letters.index do |letter|
-      prefix_sums[letter][from] - prefix_sums[letter][to+1] < 0
-    end + 1
+  output = []
+  p.zip(q).each do |from, to|
+    letters.keys.each do |letter|
+      if prefix_sums[letter][from] != prefix_sums[letter][to+1]
+        output << letters[letter]
+        break
+      end
+    end
   end
+
+  output
 end
 
 require 'minitest/autorun'
 
 class Tests < MiniTest::Unit::TestCase
   def test_example_input
-    assert_equal [1, 1, 2, 4], genomic_range_query('GACACCATA', [0, 0, 4, 7], [8, 2, 5, 7])
+    assert_equal [2, 4, 1], genomic_range_query('CAGCCTA', [2, 5, 0], [4, 5, 6])
   end
 end
