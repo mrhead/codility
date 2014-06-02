367449a8dc5874d2a225fefa7ea3286ca23e8970
Patrik Bóna <patrik.bona@mrhead.sk>
2014-06-02 14:44:01 +0200

    Lesson 5 - Brackets (from scratch)

--- brackets.rb
+++ brackets.rb
@@ -1,29 +1,44 @@
 def brackets(s)
+  pairs = {
+    '{' => '}',
+    '[' => ']',
+    '(' => ')'
+  }
+
   stack = []
 
-  s.chars.each do |c|
-    return 0 if c =~ /[}\]\)]/ && stack.pop != c
-
-    stack.push('}') if c == '{'
-    stack.push(']') if c == '['
-    stack.push(')') if c == '('
+  s.chars.each do |char|
+    if pairs.keys.include?(char)
+      stack << char
+    else
+      return 0 if pairs[stack.pop] != char
+    end
   end
 
-  stack.empty? ? 1 : 0
+  return 0 if stack.any?
+  1
 end
 
 require 'minitest/autorun'
 
 class Tests < MiniTest::Unit::TestCase
-  def test_example_input_nested
+  def test_example_input
     assert_equal 1, brackets('{[()()]}')
   end
 
-  def test_example_input_not_nested
+  def test_not_properly_nested
     assert_equal 0, brackets('([)()]')
   end
 
   def test_not_closed
-    assert_equal 0, brackets('(')
+    assert_equal 0, brackets('([()]){')
+  end
+
+  def test_empty_string
+    assert_equal 1, brackets('')
+  end
+
+  def test_not_opened
+    assert_equal 0, brackets(')()')
   end
 end
