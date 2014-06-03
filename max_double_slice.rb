7824d355a230676cb007f169a9bdffa65a87ea89
Patrik Bóna <patrik.bona@mrhead.sk>
2014-06-03 12:32:03 +0200

    Lesson 7 - MaxDoubleSliceSum (from scratch)

--- max_double_slice.rb
+++ max_double_slice.rb
@@ -1,27 +1,29 @@
-# based on: http://stackoverflow.com/questions/20660989/max-double-slice-sum/20661624#20661624
-# solved with banging my head against the wall
 def max_double_slice(a)
-  max_double_slice = 0
-  max_sum_ending_at = []
-  max_sum_starting_from = []
+  a[0] = 0
+  a[-1] = 0
 
-  a[0] = a[-1] = 0
-
-  max_sum_ending_at = max_sum_ending_at_for(a)
-  max_sum_starting_from = max_sum_ending_at_for(a.reverse).reverse
-
-  for i in 1..(a.size-2)
-    current_double_slice = max_sum_ending_at[i-1] + max_sum_starting_from[i+1]
-    max_double_slice = current_double_slice if current_double_slice > max_double_slice
-  end
-  max_double_slice
-end
-
-def max_sum_ending_at_for(array)
+  max_from_left = []
   max_ending = 0
-  array.each_with_index.map do |v, i|
-    max_ending = 0 > max_ending + v ? 0 : max_ending + v
+  a.each do |v|
+    max_ending = [0, max_ending + v].max
+    max_from_left << max_ending
   end
+
+  max_from_right = []
+  max_ending = 0
+  a.reverse.each do |v|
+    max_ending = [0, max_ending + v].max
+    max_from_right << max_ending
+  end
+  max_from_right.reverse!
+
+  n = a.size
+  max_double_slice = 0
+  for i in 1..n-2
+    max_double_slice = [max_double_slice, max_from_left[i - 1] + max_from_right[i + 1]].max
+  end
+
+  max_double_slice
 end
 
 require 'minitest/autorun'
@@ -31,7 +33,11 @@ class Tests < MiniTest::Unit::TestCase
     assert_equal 17, max_double_slice([3, 2, 6, -1, 4, 5, -1, 2])
   end
 
-  def test_successive_negative_numbers
-    assert_equal 1, max_double_slice([0, 1, -10, -10, -100, 1, 9])
+  def test_negative_numbers
+    assert_equal 0, max_double_slice([-1, -1, -1, -10, -4])
+  end
+
+  def test_three_values
+    assert_equal 0, max_double_slice([10, 20, 30])
   end
 end
