252338af370d21c792c4f4b7d1e0d921eb5620d2
Patrik Bóna <patrik.bona@mrhead.sk>
2014-06-04 11:35:00 +0200

    Lesson 8 - Peaks (from scratch)

--- peaks.rb
+++ peaks.rb
@@ -1,27 +1,25 @@
-# http://stackoverflow.com/questions/20886486/codility-peaks-complexity
-
 def peaks(a)
   n = a.size
   peaks = []
 
-  (1..n-2).each do |i|
-    peaks << i if a[i-1] < a[i] && a[i] > a[i+1]
+  for i in 1..n-2
+    peaks << i if a[i - 1] < a[i] && a[i] > a[i + 1]
   end
 
-  (1..n).each do |block_size|
-    next if n % block_size != 0
-    block = 0
-    blocks = n / block_size
-    ok = true
+  for blocks_count in n.downto(1)
+    next if n % blocks_count != 0
+
+    block_size = n / blocks_count
+    current_block = 0
+
     peaks.each do |peak|
-      if peak / block_size > block
-        ok = false
+      if peak / block_size > current_block
+        # current block does't have peak
         break
       end
-      block += 1 if peak / block_size == block
+      current_block += 1 if peak / block_size == current_block
     end
-    ok = false if block != blocks
-    return blocks if ok
+    return blocks_count if current_block == blocks_count
   end
   0
 end
@@ -31,5 +29,13 @@ require 'minitest/autorun'
 class Tests < MiniTest::Unit::TestCase
   def test_example_input
     assert_equal 3, peaks([1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2])
+  end
+
+  def test_no_peaks
+    assert_equal 0, peaks([1, 2, 3, 4, 5])
+  end
+
+  def test_one_block
+    assert_equal 1, peaks([1, 2, 3, 4, 3, 3, 1, 2])
   end
 end
